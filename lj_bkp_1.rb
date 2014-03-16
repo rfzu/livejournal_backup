da05603c0fc019635b0c8c2df8c811335359cfd8
rfzu <rall@list.ru>
2014-03-17 00:18:19 +0400

    Чуть причесал

--- lj_bkp_1.rb
+++ lj_bkp_1.rb
@@ -3,33 +3,27 @@ require 'mechanize'
 
 class Lj_bckp
 
-  def initialize(login ="borisakunin", password = "")
+  def initialize(login ="technobastard", password = "")
     @login = login
     @password = password
+    @lp = Mechanize.new
+    @url = "http://#{@login}.livejournal.com/latest"
+    @page = @lp.get @url
   end
 
   def lj_login
 	end
 
 	def lj_save
-    lp = Mechanize.new
-    url = "http://#{@login}.livejournal.com/latest"
-    page = lp.get url
 
     loop do
-      #page.save 'class_.html'
-      text_url = page.uri.to_s.scan(%r{com/(.*)}).join
-      page.save text_url
-      #p text_url
-      later_page = page.link_with(:text => "Previous Entry")
-      #text = page.links.each {|link| puts link.text }
-      #p text.join
+      text_url = @page.uri.to_s.scan(%r{com/(.*)}).join
+      @page.save text_url
+      later_page = @page.link_with(:text => "Previous Entry") 
+      later_page ||= @page.link_with(:text => "Previous")
       p later_page
       break if later_page.nil?
-      #p later_page
-      page = page.link_with(:text => "Previous Entry").click
-      #page.save 'class_.html'
-      #page.links.each {|link| puts link.text }
+      @page = later_page.click
 	  end
 
   end
@@ -37,22 +31,4 @@ class Lj_bckp
 end
 
 tech = Lj_bckp.new
-tech.lj_save
-
-#наверху красивый класс, внизу грязные эксперименты
-=begin
- login = "technobastard"
- lp = Mechanize.new
- url = "http://#{login}.livejournal.com/latest"
- page = lp.get url
- #p page
- #page.save 'last1.html'
- #lp.click(upload_page.link_with(:text => /basic Uploader/))
-
- #page.links.each {|link| puts link.text }
- page =  page.link_with(:text => "Previous").click
- #p old.body
- page.save 'last3.html'
-=end
-#конец
-#new_url = text_url.scan(%r{com/(.*)}).join
+tech.lj_savep
